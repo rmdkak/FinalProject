f14018650943e0fb817c93804731d034a6b695fe
youjunho613 <njn613@gmail.com>
2023-08-20 22:37:37 +0900

    chore: header 임시 버튼 구성
    로그인, 로그아웃, 마이페이지 버튼 임시 구성

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,70 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { logout } from 'api/supabase';
+import { useAuthStore } from 'store';
 
 export const Header = () => {
-  return <header>Header</header>;
+  const navigate = useNavigate();
+
+  const { currentSession } = useAuthStore();
+  const userUid = currentSession === null ? null : currentSession.user.id;
+
+  // 로그아웃
+  const logoutHandler = async () => {
+    try {
+      await logout();
+      alert(`임시
+      로그아웃 완료`);
+    } catch (error) {
+      console.error(error);
+      // TODO 커스텀alert 로직
+    }
+  };
+
+  // 마이페이지 이동
+  const goToMypage = () => {
+    if (userUid == null) return;
+    navigate(`/mypage/${userUid}`);
+  };
+
+  return (
+    <header>
+      <button
+        className='px-4 py-2 font-semibold text-white bg-teal-400 rounded-lg shadow-md hover:bg-teal-600 focus:outline-none focus:ring focus:border-blue-300'
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        Header
+      </button>
+      {currentSession === null ? (
+        <>
+          <button
+            onClick={() => {
+              navigate('/login');
+            }}
+            className='px-4 py-2 font-semibold text-white bg-teal-400 rounded-lg shadow-md hover:bg-teal-600 focus:outline-none focus:ring focus:border-blue-300'
+          >
+            로그인
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            className='px-4 py-2 font-semibold text-white bg-teal-400 rounded-lg shadow-md hover:bg-teal-600 focus:outline-none focus:ring focus:border-blue-300'
+            onClick={logoutHandler}
+          >
+            임시_로그아웃_버튼
+          </button>
+
+          <button
+            className='px-4 py-2 font-semibold text-white bg-teal-400 rounded-lg shadow-md hover:bg-teal-600 focus:outline-none focus:ring focus:border-blue-300'
+            onClick={goToMypage}
+          >
+            임시_마이페이지_버튼
+          </button>
+        </>
+      )}
+    </header>
+  );
 };
