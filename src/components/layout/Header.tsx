3b1dfb26a90aad447dccbcb77c51d810dce6979c
muzi55 <muzi5@kakao.com>
2023-08-21 18:13:36 +0900

    Feat: header sidebar toggle menu is complete
    isOpen state로 사이드바를 오픈, 오프합니다.
    페이지 라우트 연결은 Link로 했습니다. 그 이유는 버튼을눌러서 이동하는것보다 a태그로 이동하는편이 시멘틱하다고 생각합니다.
    로그인 되었을때 로그인/ 회원가입 문구랑, link 주소를 바꿔줘야합니다.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import { logout } from "api/supabase";
+import Sidebar from "components/sidebar/Sidebar";
 import hambergerMenu from "images/header/hamburgerMenu.svg";
 import logOutIcon from "images/header/logout.svg";
 import userIcon from "images/header/user.svg";
 import { useAuthStore } from "store";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const { currentSession } = useAuthStore();
@@ -30,40 +33,50 @@ export const Header = () => {
     navigate(`/mypage/${userUid}`);
   };
 
+  const openSideBarHandler = (): void => {
+    setIsOpen(true);
+  };
   return (
-    <header className="flex justify-between items-center px-[2.75rem]">
-      <Link to="/" className="py-6 font-title">
-        STILE
-      </Link>
+    <>
+      <header className="flex justify-between items-center px-[5rem]">
+        <Link to="/" className="py-6 font-title text-[2rem]">
+          STILE
+        </Link>
 
-      {currentSession === null ? (
-        <>
-          {/* 로그인 안되어있는 메뉴 */}
-          <button>
-            <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]"></span>
-            <img src={hambergerMenu} alt="햄버거메뉴 이미지" />
-          </button>
-        </>
-      ) : (
-        <>
-          {/* 로그인 되어있는 메뉴 */}
-          <div className="flex items-center justify-center">
-            <button className="mr-2" onClick={logoutHandler}>
-              <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">로그아웃버튼</span>
-              <img src={logOutIcon} alt="로그아웃" />
+        {currentSession === null ? (
+          <>
+            {/* 로그인 안되어있는 메뉴 */}
+            <button onClick={openSideBarHandler}>
+              <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]"></span>
+              <img src={hambergerMenu} alt="햄버거메뉴 이미지" />
             </button>
+          </>
+        ) : (
+          <>
+            {/* 로그인 되어있는 메뉴 */}
+            {/* 로그아웃 */}
+            <div className="flex items-center justify-center">
+              <button className="mr-2" onClick={logoutHandler}>
+                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">로그아웃버튼</span>
+                <img src={logOutIcon} alt="로그아웃" />
+              </button>
 
-            <button className="mr-2" onClick={goToMypage}>
-              <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">마이페이지버튼</span>
-              <img src={userIcon} alt="마이 페이지" />
-            </button>
-            <button className="">
-              <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">햄버거</span>
-              <img src={hambergerMenu} alt="햄버거 메뉴" />
-            </button>
-          </div>
-        </>
-      )}
-    </header>
+              {/* 마이페이지 */}
+              <button className="mr-2" onClick={goToMypage}>
+                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">마이페이지버튼</span>
+                <img src={userIcon} alt="마이 페이지" />
+              </button>
+
+              {/* 햄버거 */}
+              <button onClick={openSideBarHandler} className="">
+                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">햄버거</span>
+                <img src={hambergerMenu} alt="햄버거 메뉴" />
+              </button>
+            </div>
+          </>
+        )}
+      </header>
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
   );
 };
