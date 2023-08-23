98775e8bce4847c97f758bf3414c1f36c2fa93fd
Jinsu Kim <kimjinsu0210@naver.com>
2023-08-23 09:24:06 +0900

    feat: 게시물 slide, pagination 기능 추가 1. slide 는egjs 의 flicking 사용 2. pagination 은 라이브러리 없이 코드로 구현
    design: community 게시판 UI 새로 변경된 와이어프레임 대로 수정
    
    fix: 게시물 option 변경 시 페이지 초기값이 1이 안되는 버그 수정

--- src/components/mypage/MenuTab.tsx
+++ src/components/mypage/MenuTab.tsx
@@ -13,6 +13,7 @@ const getMyPosts: Array<Tables<"POSTS">> = [
     id: "uuid",
     tileId: "uuid",
     wallpaperId: "uuid",
+    nickname: "유준호",
   },
 ];
 
---
+++ src/components/pagination/PostPagination.tsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+import { POSTS_PER_PAGE } from "pages";
+
+interface PaginationProps {
+  totalPosts: number;
+  paginate: (pageNumber: number) => void;
+}
+
+export const PostPagination = ({ totalPosts, paginate }: PaginationProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(totalPosts / POSTS_PER_PAGE);
+  const pagesToShow = 3;
+
+  const showPage = (pageNumber: number) => {
+    if (pageNumber >= 1 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+      paginate(pageNumber);
+    }
+  };
+  useEffect(() => {
+    console.log("1",1)
+    setCurrentPage(1);
+  }, [totalPosts]);
+  const showPrevPage = () => {
+    showPage(currentPage - 1);
+  };
+  const showNextPage = () => {
+    showPage(currentPage + 1);
+  };
+
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    const currentPageGroup = Math.ceil(currentPage / pagesToShow);
+    const startPage = (currentPageGroup - 1) * pagesToShow + 1;
+
+    for (let i = startPage; i <= Math.min(startPage + pagesToShow - 1, totalPages); i++) {
+      pageNumbers.push(i);
+    }
+
+    return pageNumbers;
+  };
+  const selectedPageColor = (number: number) => {
+    if (number === currentPage) {
+      return "text-[#000000]";
+    } else {
+      return "text-[#e6e6e6]";
+    }
+  };
+  return (
+    <ul className="relative flex gap-3">
+      <IoIosArrowBack className="text-[20px] cursor-pointer" onClick={showPrevPage} />
+
+      {getPageNumbers().map((number) => (
+        <li key={number}>
+          <button
+            onClick={() => {
+              showPage(number);
+            }}
+            className={`w-[25px] font-bold ${selectedPageColor(number)}`}
+          >
+            {number}
+          </button>
+        </li>
+      ))}
+      <IoIosArrowForward className="text-[20px] cursor-pointer" onClick={showNextPage} />
+    </ul>
+  );
+};
