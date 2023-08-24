f5c55ffc2fdf5e258366265366649a56a0b43779
rmdkak <rmdkak99786@naver.com>
2023-08-24 11:37:32 +0900

    design: home wire frame 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
+import { RxChevronRight } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
 
 import { Arrow } from "@egjs/flicking-plugins";
 import Flicking, { ViewportSlot } from "@egjs/react-flicking";
-import "@egjs/react-flicking/dist/flicking.css";
-import "@egjs/flicking-plugins/dist/arrow.css";
 
 export const Home = () => {
   const navigate = useNavigate();
@@ -11,50 +10,127 @@ export const Home = () => {
 
   return (
     <>
-      <div className="w-full h-[800px] bg-gray-400 mb-40"></div>
-      <div className="flex flex-col gap-8 mx-20 mb-40">
+      <div className="w-full h-[968px] mb-40">
+        <div className="flex justify-between mx-20 mb-20">
+          <div className="ml-20">
+            <h1 className="text-[80px]">
+              내 스타일 그대로,
+              <br />
+              인테리어를 완성해보세요!
+            </h1>
+            <p className="text-xl text-[#666]">
+              나만의 조합으로 완성되는, 소중한 우리 공간! 당신의 취향이 만들어가는 따뜻한 인테리어 세상에 오세요.
+            </p>
+          </div>
+          <div className="flex flex-col mt-auto">
+            <span className="-rotate-90 text-[#9A9A9A] -translate-y-3">scroll</span>
+            <span className="h-20 border-r-[1px] border-[#9A9A9A] -translate-x-[18px]" />
+          </div>
+        </div>
+        <Flicking align={"prev"} circular={true} autoResize={false}>
+          <div className="w-[800px] h-[450px] bg-slate-600 rounded-xl mx-5"></div>
+          <div className="w-[800px] h-[450px] bg-slate-600 rounded-xl mx-5"></div>
+          <div className="w-[800px] h-[450px] bg-slate-600 rounded-xl mx-5"></div>
+          <div className="w-[800px] h-[450px] bg-slate-600 rounded-xl mx-5"></div>
+          <div className="w-[800px] h-[450px] bg-slate-600 rounded-xl mx-5"></div>
+        </Flicking>
+      </div>
+      <div className="flex flex-col gap-10 mx-20 mb-40">
         <div className="flex justify-between">
           <h1 className="text-3xl font-semibold">한눈에 보는 우리집</h1>
-          <button
-            onClick={() => {
-              navigate("/service");
-            }}
-          >
-            VIEW MORE {`>`}
-          </button>
+          <div>
+            <label htmlFor="moveToService" className="hover:cursor-pointer">
+              VIEW MORE
+            </label>
+            <button
+              id="moveToService"
+              onClick={() => {
+                navigate("/service");
+              }}
+            >
+              <RxChevronRight className="text-xl translate-y-1" />
+            </button>
+          </div>
         </div>
-        <div className="flex gap-10">
-          <div className="w-[820px] h-[460px] bg-gray-400"></div>
-          <div className="flex flex-col gap-24">
-            <div>
+        <div className="flex gap-20">
+          <div className="w-[880px] h-[538px] bg-gray-400"></div>
+          <div className="flex flex-col gap-20">
+            <div className="flex flex-col gap-6">
               <p>내 마음대로 꾸미고 조합해보세요.</p>
-              <br />
-              <h2 className="text-5xl leading-[70px]">
-                자유롭게 조합하고
+              <h2 className="text-[32px] leading-[41px]">
+                디자인과 색상까지, 마음대로 설정 가능한
                 <br />
-                확인할 수 있는 인테리어
+                나만의 맞춤형 스타일링!
               </h2>
             </div>
             <div className="flex flex-col gap-6">
-              <div className="flex gap-3 text-gray-400">
-                <span>벽지</span>|<span>바닥재</span>
+              <div className="flex gap-6 text-gray-400">
+                <button className="w-28 h-12 border-[1px] border-[#d5d5d5] rounded-full hover:bg-[#FFDF7C] hover:text-black">
+                  벽지
+                </button>
+                <button className="w-28 h-12 border-[1px] border-[#d5d5d5] rounded-full hover:bg-[#FFDF7C] hover:text-black">
+                  바닥재
+                </button>
               </div>
-              <ul className="flex gap-4">
-                <li className="w-32 h-32 bg-gray-200"></li>
-                <li className="w-32 h-32 bg-gray-200"></li>
-                <li className="w-32 h-32 bg-gray-200"></li>
-                <li className="w-32 h-32 bg-gray-200"></li>
-                <li className="w-32 h-32 bg-gray-200"></li>
-                <li className="w-32 h-32 bg-gray-200"></li>
+              <ul className="flex flex-wrap gap-4 w-[800px]">
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
+                <li className="w-[120px] h-[120px] rounded-xl bg-gray-200"></li>
               </ul>
             </div>
           </div>
         </div>
       </div>
+      <div className="flex flex-col gap-10 mx-20 mb-40">
+        <h1 className="text-3xl font-semibold">BEST COLORS</h1>
+        <ul className="flex">
+          <li className="flex">
+            <p className="text-2xl mr-7">1</p>
+            <div className="w-40 h-40 rounded-xl bg-slate-600"></div>
+            <div className="z-10 w-40 h-40 -translate-x-20 translate-y-10 rounded-xl bg-slate-400"></div>
+          </li>
+          <li className="flex">
+            <p className="text-2xl mr-7">2</p>
+            <div className="w-40 h-40 rounded-xl bg-slate-600"></div>
+            <div className="z-10 w-40 h-40 -translate-x-20 translate-y-10 rounded-xl bg-slate-400"></div>
+          </li>
+          <li className="flex">
+            <p className="text-2xl mr-7">3</p>
+            <div className="w-40 h-40 rounded-xl bg-slate-600"></div>
+            <div className="z-10 w-40 h-40 -translate-x-20 translate-y-10 rounded-xl bg-slate-400"></div>
+          </li>
+          <li className="flex">
+            <p className="text-2xl mr-7">4</p>
+            <div className="w-40 h-40 rounded-xl bg-slate-600"></div>
+            <div className="z-10 w-40 h-40 -translate-x-20 translate-y-10 rounded-xl bg-slate-400"></div>
+          </li>
+          <li className="flex">
+            <p className="text-2xl mr-7">5</p>
+            <div className="w-40 h-40 rounded-xl bg-slate-600"></div>
+            <div className="z-10 w-40 h-40 -translate-x-20 translate-y-10 rounded-xl bg-slate-400"></div>
+          </li>
+        </ul>
+      </div>
       <div className="flex flex-col gap-8 mx-20 mb-40">
         <div className="flex justify-between">
           <h1 className="text-3xl font-semibold">EVENT</h1>
-          <button>VIEW MORE {`>`}</button>
+          <div>
+            <label htmlFor="moveToEvent" className="hover:cursor-pointer">
+              VIEW MORE
+            </label>
+            <button id="moveToEvent">
+              <RxChevronRight className="text-xl translate-y-1" />
+            </button>
+          </div>
         </div>
         <div className="flex gap-10">
           <div className="flex flex-col w-[860px] h-[570px] gap-10">
@@ -76,13 +152,19 @@ export const Home = () => {
       <div className="flex flex-col gap-8 mx-20 mb-40">
         <div className="flex justify-between">
           <h1 className="text-3xl font-semibold">COMMUNTY</h1>
-          <button
-            onClick={() => {
-              navigate("/community");
-            }}
-          >
-            VIEW MORE {`>`}
-          </button>
+          <div>
+            <label htmlFor="moveToCommunity" className="hover:cursor-pointer">
+              VIEW MORE
+            </label>
+            <button
+              id="moveToCommunity"
+              onClick={() => {
+                navigate("/community");
+              }}
+            >
+              <RxChevronRight className="text-xl translate-y-1" />
+            </button>
+          </div>
         </div>
         <Flicking align={"prev"} circular={true} panelsPerView={3} plugins={plugins}>
           <div className="w-[520px] h-[254px] bg-gray-200 mx-5"></div>
@@ -91,8 +173,8 @@ export const Home = () => {
           <div className="w-[520px] h-[254px] bg-gray-200 mx-5"></div>
           <div className="w-[520px] h-[254px] bg-gray-200 mx-5"></div>
           <ViewportSlot>
-            <span className="flicking-arrow-prev is-thin"></span>
-            <span className="flicking-arrow-next is-thin"></span>
+            <span className="flicking-arrow-prev is-circle"></span>
+            <span className="flicking-arrow-next is-circle"></span>
           </ViewportSlot>
         </Flicking>
       </div>
