429583006a25fb8d8eccb3303636fd7ec36a1deb
youjunho613 <njn613@gmail.com>
2023-08-25 14:42:59 +0900

    fix: user data 테이블에서 GET
    design: mypage 아이콘 추가
    
    현재 로그인한 유저 정보 id와 매칭된 데이터 테이블에서 정보 가져올 수 있도록 수정

--- src/components/mypage/MenuTab.tsx
+++ src/components/mypage/MenuTab.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { AiOutlineComment, AiOutlineUser } from "react-icons/ai";
+import { RxBookmark, RxPencil2 } from "react-icons/rx";
 
 import { MyBookmarks, MyComments, MyInfo, MyPosts } from "components/mypage";
 import { type Tables } from "types/supabase";
@@ -36,17 +38,22 @@ export const MenuTab = () => {
   const [currentTab, clickTab] = useState(0);
 
   const menuArray = [
-    { name: "내가 쓴 글", icon: { svg: "", alt: "내가 쓴 글" }, component: <MyPosts />, data: getMyPosts },
-    { name: "내가 쓴 댓글", icon: { svg: "", alt: "내가 쓴 댓글" }, component: <MyComments />, data: getMyComments },
-    { name: "북마크", icon: { svg: "", alt: "북마크" }, component: <MyBookmarks />, data: getMyBookmarks },
-    { name: "내 정보", icon: { svg: "", alt: "내 정보" }, component: <MyInfo /> },
+    { name: "내가 쓴 글", icon: <RxPencil2 className="text-[25px]" />, component: <MyPosts />, data: getMyPosts },
+    {
+      name: "내가 쓴 댓글",
+      icon: <AiOutlineComment className="text-[25px]" />,
+      component: <MyComments />,
+      data: getMyComments,
+    },
+    { name: "북마크", icon: <RxBookmark className="text-[25px]" />, component: <MyBookmarks />, data: getMyBookmarks },
+    { name: "내 정보", icon: <AiOutlineUser className="text-[25px]" />, component: <MyInfo /> },
   ];
 
   const selectMenuHandler = (index: number) => {
     clickTab(index);
   };
 
-  const liCommonStyle = "flex flex-col items-center gap-[16px] w-[146px]";
+  const liCommonStyle = "flex flex-col items-center gap-[16px] w-[146px] cursor-pointer";
   const liSelectStyle = "text-gray-400";
   const menuTab = menuArray.map((el, index) => (
     <>
@@ -60,7 +67,7 @@ export const MenuTab = () => {
         }}
       >
         {/* 아이콘 24px 정사각형 */}
-        <img src={el.icon.svg} alt={el.icon.alt} />
+        {el.icon}
         <p className="text-[18px] font-[400] leading-[130%]">{el.name}</p>
         {el.name === "내 정보" ? (
           <p className="text-[20px] font-[500] leading-[130%]">수정</p>
