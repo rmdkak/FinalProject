ea1eb7222bf03e01416eb1aaef0f95b7eaef35a1
rmdkak <rmdkak99786@naver.com>
2023-09-03 23:51:45 +0900

    feat: community, header, footer ui 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -41,43 +41,45 @@ export const Header = () => {
 
   return (
     <>
-      <header className="sticky z-[9100] box-border border-b border-b-gray06 top-0 left-0 w-full bg-white contents-between items-center px-[5rem]">
-        <Link to="/" className="py-6 font-title text-[2rem]">
-          STILE
-        </Link>
+      <header className="flex justify-center sticky z-[9100] box-border border-b border-b-gray06 top-0 left-0 w-full bg-white">
+        <div className="w-[1280px] contents-between items-center">
+          <Link to="/" className="py-6 font-title text-[2rem]">
+            STILE
+          </Link>
 
-        {currentSession === null ? (
-          <>
-            {/* 로그인 안되어있는 메뉴 */}
-            <button onClick={openSideBarHandler}>
-              <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]"></span>
-              <img src={hambergerMenu} alt="햄버거메뉴 이미지" />
-            </button>
-          </>
-        ) : (
-          <>
-            {/* 로그인 되어있는 메뉴 */}
-            {/* 로그아웃 */}
-            <div className="flex contents-center">
-              <button className="mr-2" onClick={logoutHandler}>
-                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">로그아웃버튼</span>
-                <img src={logOutIcon} alt="로그아웃" />
+          {currentSession === null ? (
+            <>
+              {/* 로그인 안되어있는 메뉴 */}
+              <button onClick={openSideBarHandler}>
+                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]"></span>
+                <img src={hambergerMenu} alt="햄버거메뉴 이미지" />
               </button>
+            </>
+          ) : (
+            <>
+              {/* 로그인 되어있는 메뉴 */}
+              {/* 로그아웃 */}
+              <div className="flex contents-center">
+                <button className="mr-2" onClick={logoutHandler}>
+                  <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">로그아웃버튼</span>
+                  <img src={logOutIcon} alt="로그아웃" />
+                </button>
 
-              {/* 마이페이지 */}
-              <button className="mr-2" onClick={goToMypage}>
-                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">마이페이지버튼</span>
-                <img src={userIcon} alt="마이 페이지" />
-              </button>
+                {/* 마이페이지 */}
+                <button className="mr-2" onClick={goToMypage}>
+                  <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">마이페이지버튼</span>
+                  <img src={userIcon} alt="마이 페이지" />
+                </button>
 
-              {/* 햄버거 */}
-              <button onClick={openSideBarHandler} className="">
-                <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">햄버거</span>
-                <img src={hambergerMenu} alt="햄버거 메뉴" />
-              </button>
-            </div>
-          </>
-        )}
+                {/* 햄버거 */}
+                <button onClick={openSideBarHandler} className="">
+                  <span className="absolute top-[-9999px] left-[-9999px] poindent-[-9999px]">햄버거</span>
+                  <img src={hambergerMenu} alt="햄버거 메뉴" />
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </header>
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
