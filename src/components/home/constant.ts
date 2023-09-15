313e5813749c58592bf6236f10b2e57e3a27d092
muzi55 <muzi5@kakao.com>
2023-09-15 22:20:49 +0900

    feat: home keyVisual
    메인비쥬얼 이 프리로드 적용했습니다.
    8, 18, 11 => 4, 5, 8 밀리초로 preload 적용됨

---
+++ src/components/home/constant.ts
@@ -0,0 +1,10 @@
+import img01 from "assets/kv_img01.jpg";
+import img01_webp from "assets/kv_img01_webp.webp";
+import img02 from "assets/kv_img02.jpg";
+import img02_webp from "assets/kv_img02_webp.webp";
+import img03 from "assets/kv_img03.jpg";
+import img03_webp from "assets/kv_img03_webp.webp";
+
+const homeImgArray = [img01, img01_webp, img02, img02_webp, img03, img03_webp];
+
+export { homeImgArray };
--- src/utils/imgPreload.tsx
+++ src/utils/imgPreload.tsx
@@ -1,25 +1,12 @@
-import { useLayoutEffect } from "react";
-
-interface Src {
-  src: string;
-}
-interface SrcArray {
-  src: string[];
-}
-
-const preloadImg = ({ src }: Src) => {
-  useLayoutEffect(() => {
-    const imgPreload = new Image();
-    imgPreload.src = src;
-  }, []);
+const preloadImg = (src: string) => {
+  const imgPreload = new Image();
+  imgPreload.src = src;
 };
-const preloadImgs = ({ src }: SrcArray) => {
-  useLayoutEffect(() => {
-    src.forEach((image) => {
-      const img = new Image();
-      img.src = image;
-    });
-  }, []);
+const preloadImgs = (src: string[]) => {
+  src.forEach((image) => {
+    const img = new Image();
+    img.src = image;
+  });
 };
 
 export { preloadImg, preloadImgs };
